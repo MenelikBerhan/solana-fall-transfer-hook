7bd46d420db8c1f48c1491db58923d3e3366f7af
MenelikBerhan <menelikberhan@gmail.com>
2026-09-19 01:55:12 +0300

    challenge 4
    Tests copy pasted from Satoshi-Sh

---
+++ programs/solana-fall-transfer-hook/tests/test_token_mover.rs
@@ -0,0 +1,75 @@
+#[allow(dead_code)]
+mod helpers;
+
+use {
+    solana_keypair::Keypair,
+    solana_message::{Message, VersionedMessage},
+    solana_signer::Signer,
+    solana_transaction::versioned::VersionedTransaction,
+};
+
+use helpers::{setup, setup_mint_and_extra_metas, create_ata, mint_tokens, build_transfer_via_mover_ix};
+
+#[test]
+fn test_transfer_via_mover() {
+    let (mut svm, payer, program_id) = setup();
+    let mint = Keypair::new();
+
+    setup_mint_and_extra_metas(&mut svm, &payer, &mint, &program_id);
+
+    let recipient = Keypair::new();
+    svm.airdrop(&recipient.pubkey(), 1_000_000_000).unwrap();
+
+    let source_ata = create_ata(&mut svm, &payer, &payer.pubkey(), &mint.pubkey());
+    let dest_ata = create_ata(&mut svm, &payer, &recipient.pubkey(), &mint.pubkey());
+
+    mint_tokens(&mut svm, &payer, &mint.pubkey(), &source_ata, 1_000_000);
+
+    let transfer_ix = build_transfer_via_mover_ix(
+        &source_ata, &dest_ata, &mint.pubkey(), &payer.pubkey(),
+        &program_id, &token_mover::id(), 100, 9,
+    );
+
+    let blockhash = svm.latest_blockhash();
+    let msg = Message::new_with_blockhash(&[transfer_ix], Some(&payer.pubkey()), &blockhash);
+    let tx = VersionedTransaction::try_new(VersionedMessage::Legacy(msg), &[&payer]).unwrap();
+
+    let res = svm.send_transaction(tx);
+    assert!(res.is_ok(), "Transfer through token-mover failed: {:?}", res.err());
+}
+
+#[test]
+fn test_transfer_via_mover_rate_limit_exceeded() {
+    let (mut svm, payer, program_id) = setup();
+    let mint = Keypair::new();
+
+    setup_mint_and_extra_metas(&mut svm, &payer, &mint, &program_id);
+
+    let recipient = Keypair::new();
+    svm.airdrop(&recipient.pubkey(), 1_000_000_000).unwrap();
+
+    let source_ata = create_ata(&mut svm, &payer, &payer.pubkey(), &mint.pubkey());
+    let dest_ata = create_ata(&mut svm, &payer, &recipient.pubkey(), &mint.pubkey());
+
+    mint_tokens(&mut svm, &payer, &mint.pubkey(), &source_ata, 2_000_000);
+
+    let ix1 = build_transfer_via_mover_ix(
+        &source_ata, &dest_ata, &mint.pubkey(), &payer.pubkey(),
+        &program_id, &token_mover::id(), 1_000_000, 9,
+    );
+    let blockhash = svm.latest_blockhash();
+    let msg = Message::new_with_blockhash(&[ix1], Some(&payer.pubkey()), &blockhash);
+    let tx = VersionedTransaction::try_new(VersionedMessage::Legacy(msg), &[&payer]).unwrap();
+    let res = svm.send_transaction(tx);
+    assert!(res.is_ok(), "Transfer at limit should succeed: {:?}", res.err());
+
+    let ix2 = build_transfer_via_mover_ix(
+        &source_ata, &dest_ata, &mint.pubkey(), &payer.pubkey(),
+        &program_id, &token_mover::id(), 1, 9,
+    );
+    let blockhash = svm.latest_blockhash();
+    let msg = Message::new_with_blockhash(&[ix2], Some(&payer.pubkey()), &blockhash);
+    let tx = VersionedTransaction::try_new(VersionedMessage::Legacy(msg), &[&payer]).unwrap();
+    let res = svm.send_transaction(tx);
+    assert!(res.is_err(), "Transfer exceeding rate limit through mover should fail");
+}
